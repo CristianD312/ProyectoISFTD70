c6faa5b55afdb58dfd993523b07db4134572dbe2
devmobi1812 <144266743+devmobi1812@users.noreply.github.com>
2023-09-15 18:25:33 -0300

    mostrarAccesorios

--- src/main/java/Pantallas/PantallaReserva.java
+++ src/main/java/Pantallas/PantallaReserva.java
@@ -5,6 +5,7 @@
 package Pantallas;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
@@ -56,8 +57,9 @@ public class PantallaReserva extends javax.swing.JFrame {
         jTable2 = new javax.swing.JTable();
         jDateChooser1 = new com.toedter.calendar.JDateChooser();
         jLabel7 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton2 = new javax.swing.JButton();
+        accesoriosSalon = new javax.swing.JTextField();
+        mostrarAccesorios = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         iniciarSesion = new javax.swing.JMenu();
         configuracion = new javax.swing.JMenu();
@@ -91,23 +93,23 @@ public class PantallaReserva extends javax.swing.JFrame {
 
         tablaAccesorios.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {"Proyector", null, null},
-                {"TV", null, null},
-                {"Cable VGA", null, null},
-                {"Cable HDMI", null, null},
-                {"Cable Interlock 220V", null, null},
-                {"Cable Audio", null, null},
-                {"Adaptador de Señal", null, null}
+                {"Proyector", null},
+                {"TV", null},
+                {"Cable VGA", null},
+                {"Cable HDMI", null},
+                {"Cable Interlock 220V", null},
+                {"Cable Audio", null},
+                {"Adaptador de Señal", null}
             },
             new String [] {
-                "Accesorios", "Si", "No"
+                "Accesorios", "Si/No"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.Boolean.class, java.lang.Boolean.class
+                java.lang.String.class, java.lang.Boolean.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false
+                false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -141,9 +143,17 @@ public class PantallaReserva extends javax.swing.JFrame {
         });
         jScrollPane2.setViewportView(jTable2);
 
-        jLabel7.setText("Accesorios del salon:");
+        jLabel7.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel7.setText("ACCESORIOS DE LOS SALONES");
 
-        jButton2.setText("Mostrar");
+        mostrarAccesorios.setText("Mostrar");
+        mostrarAccesorios.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mostrarAccesoriosActionPerformed(evt);
+            }
+        });
+
+        jLabel8.setText("Ingrese el numero del salon:");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -153,7 +163,16 @@ public class PantallaReserva extends javax.swing.JFrame {
                 .addGap(26, 26, 26)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel8)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(accesoriosSalon, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(mostrarAccesorios))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel4)
@@ -161,7 +180,7 @@ public class PantallaReserva extends javax.swing.JFrame {
                                     .addComponent(jLabel6))
                                 .addGap(18, 18, 18)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(profesorBox, 0, 165, Short.MAX_VALUE)
+                                    .addComponent(profesorBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                     .addComponent(horario)
                                     .addComponent(jDateChooser1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                             .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -173,29 +192,22 @@ public class PantallaReserva extends javax.swing.JFrame {
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(carrerasBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                     .addComponent(salonesBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
-                        .addGap(39, 39, 39))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel7)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton2)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 966, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 966, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(27, 27, 27))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(17, 17, 17)
                 .addComponent(jLabel1)
-                .addGap(24, 24, 24)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2)
                             .addComponent(salonesBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -215,17 +227,22 @@ public class PantallaReserva extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel6)
                             .addComponent(horario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(38, 38, 38)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel7)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jButton2))
+                        .addGap(31, 31, 31)
+                        .addComponent(jLabel7)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(accesoriosSalon, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(mostrarAccesorios)
+                            .addComponent(jLabel8))
+                        .addGap(18, 18, 18)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1))
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 623, Short.MAX_VALUE))
-                .addGap(31, 31, 31))
+                        .addComponent(jButton1)
+                        .addGap(31, 31, 31))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 654, Short.MAX_VALUE)
+                        .addContainerGap())))
         );
 
         iniciarSesion.setText("Iniciar Sesion");
@@ -263,6 +280,12 @@ public class PantallaReserva extends javax.swing.JFrame {
     private void salonesBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salonesBoxActionPerformed
 
     }//GEN-LAST:event_salonesBoxActionPerformed
+
+    private void mostrarAccesoriosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mostrarAccesoriosActionPerformed
+       int numeroSalon = Integer.parseInt(accesoriosSalon.getText());
+        mostrarAccesorios(numeroSalon);
+       
+    }//GEN-LAST:event_mostrarAccesoriosActionPerformed
 
     private void cargarSalonesBOX(JComboBox salonesBox){
         Conexion conect = new Conexion(null);
@@ -316,10 +339,22 @@ public class PantallaReserva extends javax.swing.JFrame {
     private void mostrarAccesorios(int numSalon){
         DefaultTableModel tAccesorios = new DefaultTableModel();
         tAccesorios.addColumn("Accesorios");
-        tAccesorios.addColumn("Si");
-        tAccesorios.addColumn("No");
+        tAccesorios.addColumn("Si/No");
+        //tAccesorios.addColumn("No");
         tablaAccesorios.setModel(tAccesorios);
-        int [] datos = new int[7];
+        
+        ArrayList<Object> datosBooleanosProyector = new ArrayList<>();
+        ArrayList<Object> datosBooleanosTV = new ArrayList<>();
+        ArrayList<Object> datosBooleanosVGA = new ArrayList<>();
+        ArrayList<Object> datosBooleanosHDMI = new ArrayList<>();
+        ArrayList<Object> datosBooleanosI220V = new ArrayList<>();
+        ArrayList<Object> datosBooleanosAUDIO = new ArrayList<>();
+        ArrayList<Object> datosBooleanosASEÑAL = new ArrayList<>();
+        
+        String verdadero = "Si";
+        String falso = "No";
+        
+        
         Conexion conect = new Conexion(null);
         conect.conectar();
         try {
@@ -327,14 +362,35 @@ public class PantallaReserva extends javax.swing.JFrame {
             java.sql.Statement statement = conect.getConn().createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
             while(resultSet.next()){
-                int proyector = resultSet.getInt("proyector");
-                int tv = resultSet.getInt("tv");
-                int cableVga = resultSet.getInt("vga");
-                int cableHdmi = resultSet.getInt("hdmi");
-                int cable220v = resultSet.getInt("interlock");
-                int cableAudio = resultSet.getInt("audio");
-                int adpConversor = resultSet.getInt("adp_conversor");
                 
+                 datosBooleanosProyector.add("Proyector"); // Nombre del accesorio, puedes cambiarlo según tu base de datos
+                 datosBooleanosProyector.add(resultSet.getInt("proyector") == 1 ? verdadero : falso); // 1 para "Si", 0 para "No"
+                 
+                 datosBooleanosTV.add("TV");
+                 datosBooleanosTV.add(resultSet.getInt("tv") == 1 ? verdadero : falso);
+                 
+                 datosBooleanosVGA.add("Cable VGA");
+                 datosBooleanosVGA.add(resultSet.getInt("vga") == 1 ? verdadero : falso);
+                 
+                 datosBooleanosHDMI.add("Cable HDMI");
+                 datosBooleanosHDMI.add(resultSet.getInt("hdmi") == 1 ? verdadero : falso);
+                 
+                 datosBooleanosI220V.add("Cable Interlock 220V");
+                 datosBooleanosI220V.add(resultSet.getInt("interlock") == 1 ? verdadero : falso);
+                 
+                 datosBooleanosAUDIO.add("Cable Audio");
+                 datosBooleanosAUDIO.add(resultSet.getInt("audio") == 1 ? verdadero : falso);
+                 
+                 datosBooleanosASEÑAL.add("Adaptador de Señal");
+                 datosBooleanosASEÑAL.add(resultSet.getInt("adp_conversor") == 1 ? verdadero : falso);
+                
+                tAccesorios.addRow(datosBooleanosProyector.toArray());
+                tAccesorios.addRow(datosBooleanosTV.toArray());
+                tAccesorios.addRow(datosBooleanosVGA.toArray());
+                tAccesorios.addRow(datosBooleanosHDMI.toArray());
+                tAccesorios.addRow(datosBooleanosI220V.toArray());
+                tAccesorios.addRow(datosBooleanosAUDIO.toArray());
+                tAccesorios.addRow(datosBooleanosASEÑAL.toArray());
             }
         } catch (Exception e) {
         }
@@ -345,13 +401,13 @@ public class PantallaReserva extends javax.swing.JFrame {
   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField accesoriosSalon;
     private javax.swing.JComboBox<String> carrerasBox;
     private javax.swing.JMenu configuracion;
     private javax.swing.JMenu estadistica;
     private javax.swing.JTextField horario;
     private javax.swing.JMenu iniciarSesion;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private com.toedter.calendar.JDateChooser jDateChooser1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -360,12 +416,13 @@ public class PantallaReserva extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable2;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton mostrarAccesorios;
     private javax.swing.JComboBox<String> profesorBox;
     private javax.swing.JComboBox<String> salonesBox;
     private javax.swing.JMenu soporteAyuda;
