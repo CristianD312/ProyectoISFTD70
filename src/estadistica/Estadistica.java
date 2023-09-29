78b5371a8e63d3a31564158f8cccf6b5d2669f1d
facubouzas <144275066+facubouzas@users.noreply.github.com>
2023-09-29 15:18:52 -0300

    prueba

---
+++ src/estadistica/Estadistica.java
@@ -0,0 +1,20 @@
+
+package estadistica;
+
+import com.sun.jdi.connect.spi.Connection;
+
+public class Estadistica {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+     try{
+            Conexion conexionn = new Conexion();
+            conexionn.conectar();
+        } catch (Exception e){
+            
+            
+        }
+    }
+    
+}
